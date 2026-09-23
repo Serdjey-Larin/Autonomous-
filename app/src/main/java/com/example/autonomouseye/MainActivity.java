60586cbc4bacd9ed870724d8cb5fbb1df46a7a73
Serdjey-Larin <larin_serdjey@mail.ru>
2026-09-23 19:28:53 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/example/autonomouseye/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.autonomouseye;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("AutonomousEye\nEnvironment Ready!");
+        tv.setTextSize(22);
+        tv.setPadding(48, 48, 48, 48);
+        setContentView(tv);
+    }
+}
