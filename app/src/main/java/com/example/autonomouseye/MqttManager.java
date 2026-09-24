7d64d369ad4164b92279356adb1391bf5841e9e2
Serdjey-Larin <larin_serdjey@mail.ru>
2026-09-24 10:43:16 +0300

    Fix MQTT import statements in MqttManager.java
    Updated import statements for MQTT classes to use the correct package.

--- app/src/main/java/com/example/autonomouseye/MqttManager.java
+++ app/src/main/java/com/example/autonomouseye/MqttManager.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.util.Log;
 
 import info.mqtt.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mv3.IMqttActionListener;
-import org.eclipse.paho.client.mv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mv3.IMqttToken;
-import org.eclipse.paho.client.mv3.MqttCallbackExtended;
-import org.eclipse.paho.client.mv3.MqttConnectOptions;
-import org.eclipse.paho.client.mv3.MqttException;
-import org.eclipse.paho.client.mv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.util.UUID;
 
